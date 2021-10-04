1bccdb938331093dd9c5a8372812da93aa73caff
baijingqi <baijq@bbmmgroup.com>
2021-10-04 09:19:17 +0800

    week5

--- week5/main.go
+++ week5/main.go
@@ -9,19 +9,24 @@ import (
 type limitManager struct {
     maxTicketNum       int
     serverIngTicketNum int
+    countSecond        int
     windowStartTime    int64
     lock               sync.RWMutex
 }
 
 func main() {
-    limitManager := &limitManager{maxTicketNum: 20, windowStartTime: time.Now().Unix()}
+    limitManager := &limitManager{maxTicketNum: 20, countSecond: 20, windowStartTime: time.Now().Unix()}
 
-    for i := 0; i < 60; i++ {
+    for i := 0; i < 100; i++ {
         ticket := limitManager.getTicket()
         if !ticket {
             fmt.Println("令牌数已超出")
         } else {
             fmt.Println("正常请求")
+            go func() {
+                defer limitManager.reduceServingTicket()
+                fmt.Println("do sth")
+            }()
         }
     }
 
@@ -37,8 +42,12 @@ func (l *limitManager) getTicket() bool {
     l.lock.Unlock()
 
     now := time.Now().Unix()
-    if now-l.windowStartTime >= 60 {
+    if now-l.windowStartTime >= int64(l.countSecond) {
         l.windowStartTime = now + 1
     }
     return true
+}
+
+func (l *limitManager) reduceServingTicket() {
+    l.serverIngTicketNum--
 }
